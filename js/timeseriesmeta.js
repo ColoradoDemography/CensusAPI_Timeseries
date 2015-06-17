ac51ea5c5a1b268dd79e325708fe533f6c399b2b
Daniel Trone <royhobbstn@yahoo.com>
2015-06-17 22:20:13 +0000

    add new timeseries json

--- js/timeseriesmeta.js
+++ js/timeseriesmeta.js
@@ -51,6 +51,131 @@
       {"year": "c2000", "colname": "mhi2000", "cdesc": "2000"},
       {"year": "acs0812", "colname": "mhi0812", "cdesc": "ACS0812"}      
     ]
+  },{
+  "table": "medage",
+  "description": "Median Age",
+  "type": ".1",
+  "years": [
+    {"c1980": "sf1"},
+    {"c1990": "none"},
+    {"c2000": "sf1"},
+    {"c2010": "data"},
+    {"acs0812": "none"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "medage1980", "cdesc": "1980"},
+      {"year": "c2000", "colname": "medage2000", "cdesc": "2000"},
+      {"year": "c2010", "colname": "medage2010", "cdesc": "2010"}      
+    ]
+  },{
+  "table": "pctfamhh",
+  "description": "Percent Family Households",
+  "type": "%",
+  "years": [
+    {"c1980": "sf1"},
+    {"c1990": "sf1"},
+    {"c2000": "sf1"},
+    {"c2010": "data"},
+    {"acs0812": "none"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "pctfamhh1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "pctfamhh1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "pctfamhh2000", "cdesc": "2000"},
+      {"year": "c2010", "colname": "pctfamhh2010", "cdesc": "2010"}      
+    ]
+  },{
+  "table": "pctforeign",
+  "description": "Percent Foreign Born",
+  "type": "%",
+  "years": [
+    {"c1980": "sf3"},
+    {"c1990": "sf3"},
+    {"c2000": "sf3"},
+    {"c2010": "none"},
+    {"acs0812": "data"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "pctforeign1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "pctforeign1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "pctforeign2000", "cdesc": "2000"},
+      {"year": "acs0812", "colname": "pctforeignacs0812", "cdesc": "ACS0812"}      
+    ]
+  },{
+  "table": "edu",
+  "description": "Percent Bachelors Degree or Higher",
+  "type": "%",
+  "years": [
+    {"c1980": "sf3"},
+    {"c1990": "sf3"},
+    {"c2000": "sf3"},
+    {"c2010": "none"},
+    {"acs0812": "data"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "edu1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "edu1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "edu2000", "cdesc": "2000"},
+      {"year": "acs0812", "colname": "eduacs0812", "cdesc": "ACS0812"}      
+    ]
+  },{
+  "table": "pov",
+  "description": "Percent in Poverty",
+  "type": "%",
+  "years": [
+    {"c1980": "sf3"},
+    {"c1990": "sf3"},
+    {"c2000": "sf3"},
+    {"c2010": "none"},
+    {"acs0812": "data"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "pov1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "pov1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "pov2000", "cdesc": "2000"},
+      {"year": "acs0812", "colname": "povacs0812", "cdesc": "ACS0812"}      
+    ]
+  },{
+  "table": "medval",
+  "description": "CPI Adjusted Median Home Value ($2012)",
+  "type": "$,",
+  "years": [
+    {"c1980": "sf1"},
+    {"c1990": "sf1"},
+    {"c2000": "sf3"},
+    {"c2010": "none"},
+    {"acs0812": "data"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "medval1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "medval1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "medval2000", "cdesc": "2000"},
+      {"year": "acs0812", "colname": "medvalacs0812", "cdesc": "ACS0812"}      
+    ]
+  },{
+  "table": "pct65",
+  "description": "Percent Age 65 Plus",
+  "type": "%",
+  "years": [
+    {"c1980": "sf1"},
+    {"c1990": "sf1"},
+    {"c2000": "sf1"},
+    {"c2010": "data"},
+    {"acs0812": "none"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "pct65_1980", "cdesc": "1980"},
+      {"year": "c1990", "colname": "pct65_1990", "cdesc": "1990"},    
+      {"year": "c2000", "colname": "pct65_2000", "cdesc": "2000"},
+      {"year": "c2010", "colname": "pct65_2010", "cdesc": "2010"}      
+    ]
   }
 
 
